df86ee46b5aa071ab05ef19549f15e6636d44f05
linyn-zero <1627286457@qq.com>
2022-10-20 20:15:24 +0800

    P3 final complete

--- P3-TextAnalysisTools/TextAnalyst.cpp
+++ P3-TextAnalysisTools/TextAnalyst.cpp
@@ -11,98 +11,112 @@ void TextAnalyst::perform() {
 			cout << "what to do? (选择你想进行的操作)：" << std::endl
 			     << "1 for Displaying all the words." << std::endl
 			     << "2 for Input what you want." << std::endl
+				 << "3 for check how much keyWord in the file." << std::endl
 			     << "0 for Change file." << std::endl;
-			int command = 3;
+			string command;
 			cin >> command;
-			switch (command) {
-				case 1:
-					ShowAllWord_byMAPDICT();
+			switch (command[0]) {
+				case '1':
+					ShowAllWord_byVECTOR(existing_allWords_sorted);
 					break;
-				case 2:
+				case '2':
 					InputSearch();
 					break;
-				case 0: // 退出
+				case '3':
+					showKeyword();
+					break;
+				case '0': // 退出
 					return ;
 				default:
 					cout << "Try again?" << std::endl;
 			}
 		}
 	} catch (const exception& e){
-		cout << "WRONG somewhere!" ;
+		cout << e.what() ;
 	}
 }
 void TextAnalyst::initContainer() {
-	allClear(); // 清空容器
+	clearAllContainer(); // 清空容器
 
 	cout << "Input the address:" << std::endl;
-	string fileAddr;              // 输入一个文件的地址
-	cin >> fileAddr;
-	ifstream sourse;  // 从文件中获取数据源
-	sourse.open(fileAddr); // 获取文件流
+	ifstream source;  // 从文件中获取数据源
+	string fileAddress;  // 输入一个文件的地址
+	// 文件输入的正确性检查
+	do{
+		cin >> fileAddress;
+		source.open(fileAddress); // 获取文件流c
+		if(!source.is_open()) { cout << "Tr again?" << std::endl; }
+	} while(!source.is_open());
 
 	int lineIndex = 0; // 行号
 	string isLine;
-	getline(sourse,isLine,'\n');       // 从文件流中获取一行输入string
+	getline(source, isLine, '\n');       // 从文件流中获取一行输入string
 	bool stopFlag1 = false;
+	// 将文本中的词汇信息预处理到各个类成员变量中
+	unordered_set<string> findKeyword; // 用set容器暂存keyword以防止重复
 	while(true){
 		if(!isLine.empty()) {
-//			stringstream issLine(isLine);   // 将一行输入string转化为一行输入sstream
-//		    issLine << isLine;
 			string word;
-			bool stopflag2 = false;
+			bool stopFlag2 = false;
 			trimLine2Word(isLine,word);       //修剪单词
 			while(true) {                        // 从一行输入中获取单词
 				if(!word.empty()) {
 					wordsByDict[word]++;             //单词总数++
 					wordsLine[word][lineIndex]++;    //该行单词数++
 					cout << word << " " ;
+					for(const string& keyword : keyWords){  // 将关键字预处理到成员变量Existing_keywords中
+						if(word == keyword){
+							findKeyword.insert(keyword);
+						}
+					}
 				}
-				if(stopflag2) { break; }
-				stopflag2 = !trimLine2Word(isLine,word);  //修剪单词
+				if(stopFlag2) { break; }
+				stopFlag2 = !trimLine2Word(isLine, word);  //修剪单词
 			}
 			cout << std::endl; // 检查输入是否正常
 			lineIndex++;
 		}
 		if(stopFlag1) { break; }
-		getline(sourse,isLine,'\n');   // 从文件流中获取一行输入string
-	    stopFlag1 = sourse.eof();
+		getline(source, isLine, '\n');   // 从文件流中获取一行输入string
+	    stopFlag1 = source.eof();
 	}
-}
-void TextAnalyst::ShowAllWord_byMAPDICT() const {
-    cout << std::setw(10) << std::right << "Word" << std::setw(15) << std::left << "List" << std::right << "Number" << std::endl;
-	int i = 0;
-	for(auto & str_num : wordsByDict){
-		cout << i++ << ". " <<  std::setw(25) << std::left << str_num.first << str_num.second << std::endl;
-	}
+	existing_keywords_sorted.assign(findKeyword.begin(), findKeyword.end());
+	// 预处理成员变量existing_keywords_sorted中的关键字，使existing_keywords_sorted按词频降序排列
+	std::sort(existing_keywords_sorted.begin(),
+	          existing_keywords_sorted.end(),
+	          [&](const string& a, const string& b){
+		if(wordsByDict[a]==wordsByDict[b]){
+			return a<b;
+		}
+		return wordsByDict[a] > wordsByDict[b];
+	});
 
-	// 后续服务：进一步可循环单词检索
-	int command = -2;
-	while(true){
-		cout << "Which word would you like to Search? input please." << std::endl
-		     << "(-1 for Quit | Other will loop):" ; // 询问是否想通过序号查询单词信息
-		string input;
-		cin >> input;
-		if(std::isdigit(input[0]) || input[0] == '-' && std::isdigit(input[1])){
-			command = std::stoi(input);
-		}
-		else {continue;}
-		if(command >= 0 && command < wordsByDict.size()){
-			auto itw = wordsByDict.begin();
-			while(command--) itw++;                 // 先获取序号对应的单词
-			displayTheWord(itw->first);    // 展示所选单词
-		}
-		else if(command == -1){
-			break;
-		} else{
-			continue;
-		}
+	for(const auto& item : wordsByDict ){
+		existing_allWords_sorted.emplace_back(item.first);
 	}
+	// 预处理成员变量existing_allWords_sorted中的单词，使existing_allWords_sorted按词频降序排列
+	std::sort(existing_allWords_sorted.begin(),
+			  existing_allWords_sorted.end(),
+			  [&](const string& a, const string& b){
+				  if(wordsByDict[a]==wordsByDict[b]){
+					  return a<b;
+				  }
+				  return wordsByDict[a] > wordsByDict[b];
+			  });
+}
+void TextAnalyst::showKeyword() const{
+	if(existing_keywords_sorted.empty()) { cout << "Found nothing, try again?" << std::endl; }
+	else {
+		ShowAllWord_byVECTOR(existing_keywords_sorted);
+		cout << "Please input the word you want to Search" << std::endl
+		     << "(0 for quit):";
+	} // 打印所有可能答案
 }
 void TextAnalyst::InputSearch() const {
+	cout << "Please input the word you want to Search" << std::endl
+	     << "(0 for quit):";
 	while(true){
-		cout << "Plesae input the word you want to Search" << std::endl
-		     << "(0 for quit):";
-		string target; // 输入想查的单词
+		string target;
 		cin >> target;
 
 		if(target[0] == '0') { break; } // 退出指令判断
@@ -113,31 +127,47 @@ void TextAnalyst::InputSearch() const {
 				found.emplace_back(it.first);
 			}
 		}
-		if(found.empty()) { cout << "Found nothing, will try again." << std::endl; }
-		else { ShowAllWord_byVECTOR(found); } // 打印所有可能答案
+		if(found.empty()) { cout << "Found nothing, try again?" << std::endl; }
+		else {
+			// 预处理found中的单词，使found按词频降序排列
+			std::sort(found.begin(),
+			          found.end(),
+			          [&](const string& a, const string& b){
+				          if(wordsByDict.find(a)->second == wordsByDict.find(b)->second){
+					          return a<b;
+				          }
+				          return wordsByDict.find(a)->second > wordsByDict.find(b)->second;
+			          });
+			ShowAllWord_byVECTOR(found);
+			cout << "Please input the word you want to Search" << std::endl
+			     << "(0 for quit):";
+		} // 打印所有可能答案
 	}
 }
-void TextAnalyst::ShowAllWord_byVECTOR(vector<string> strs) const {
+void TextAnalyst::ShowAllWord_byVECTOR(const vector<string>& words) const {
 	cout <<std::endl << "The Searching outcome:" << std::endl;
 	cout << std::setw(10) << std::right << "Word" << std::setw(15) << std::left << "List" << std::right << "Number" << std::endl;
 	int i = 0;
-	for(auto & word : strs){
+	for(auto & word : words){
 		cout << i++ << ". " <<  std::setw(25) << std::left << word << wordsByDict.find(word)->second << std::endl;
 	}
 
 	// 后续服务：进一步可循环单词检索
-	int command = -2;
+	int command {};
 	while(true){
 		cout << "Which word would you like to Search? input please." << std::endl
 		     << "(-1 for Quit | Other will loop): " ; // 询问是否想通过序号查询单词信息
 		string input;
-		cin >> input;
-		if(std::isdigit(input[0]) || input[0] == '-' && std::isdigit(input[1])){
-			command = std::stoi(input);
+		while(true){
+			cin >> input;
+			if(std::isdigit(input[0]) || input[0] == '-' && std::isdigit(input[1])){
+				command = std::stoi(input);
+				break;
+			}
+			else { cout << "Try again? " << std::endl; }
 		}
-		else {continue;}
-		if(command >= 0 && command < strs.size()){
-			displayTheWord(strs[command]);    // 展示所选单词
+		if(command >= 0 && command < words.size()){
+			displayTheWord(words[command]);    // 展示所选单词
 		}
 		else if(command == -1){
 			break;
@@ -172,7 +202,7 @@ int TextAnalyst::myKMP(const string& str, const string& model)  {
 	};
 
 	vector<int> next(move(getNextVal(model)));
-	int j;
+	int j = 0;
 	for(int i = 0; i < str.size(); i++){
 		while(j > 0 && model[j] != str[i]) j = next[j-1];
 		if(str[i] == model[j]) j++;
@@ -183,8 +213,9 @@ int TextAnalyst::myKMP(const string& str, const string& model)  {
 bool TextAnalyst::trimLine2Word(string &line, string &word) {
 	word.clear();
 	int index{};
-	while(index < line.size() && (std::isalpha(line[index]) || line[index] == '_')) {
-		word += tolower(line[index++]);
+	while(index < line.size() && (std::isalpha(line[index]) || line[index] == '_' || std::isdigit(line[index]))) {
+		if(std::isalpha(line[index])) { word += tolower(line[index++]); }
+		else { word += line[index++]; }
 	}
 	if(index+1 < line.size() && line[index] == '\'' && line[index + 1] == 's'){ // 处理“'s”
 		index += 2;
@@ -195,23 +226,33 @@ bool TextAnalyst::trimLine2Word(string &line, string &word) {
 	line = line.substr(index);
 	return !line.empty();
 }
-
-void TextAnalyst::allClear() {
+void TextAnalyst::clearAllContainer() {
 	wordsByDict.clear();
 	wordsLine.clear();
+	existing_keywords_sorted.clear();
+	existing_allWords_sorted.clear();
 }
 
-//void TextAnalyst::displayTheWord(int index) const {
-//	auto itw = wordsByDict.begin();
-//	while(index--) itw++;
-//	string theWord = itw->first;     // 先获取序号对应的单词
-//
-//	cout << "Word: " << theWord << std::endl;
-//	cout <<std::setw(10) << std::left << "Line" << "nums" << std::endl;
-//	auto it = wordsLine.find(theWord);
-//	for(auto item : it->second){
-//		cout << item.first << " " << item.second << std::endl;
-//	}
-//
-//}
+TextAnalyst::TextAnalyst() : keyWords({"alignas","alignof"	,"asm"	,"auto"	,"bool",
+                                       "break"	,"case"	,"catch",	"char"	,"char16_t",
+                                       "char32_t"	,"class",	"const",	"const_cast",	"constexpr",
+                                       "continue",	"decltype",	"default"	,"delete",	"do",
+                                       "double",	"dynamic_cast"	,"else"	,"enum"	,"explicit",
+                                       "export"	,"extern",	"false",	"float",	"for",
+                                       "friend",	"goto"	,"if"	,"inline"	,"int",
+                                       "long",	"mutable"	,"namespace","new"	,"noexcept",
+                                       "nullptr"	,"operator"	,"private",	"protected"	,"public",
+                                       "register"	,"reinterpret_cast",	"return",	"short",	"signed",
+                                       "sizeof",	"static",	"static_assert"	,"static_cast"	,"struct",
+                                       "switch",	"template"	,"this"	,"thread_local",	"throw",
+                                       "true",	"try",	"typedef",	"typeid"	,"typename",
+                                       "union",	"unsigned",	"using",	"virtual"	,"void",
+                                       "volatile"	,"wchar_t",	"while"}){
+	keyWords.shrink_to_fit();
+}
+
+
+
+
+
 
--- P3-TextAnalysisTools/TextAnalyst.h
+++ P3-TextAnalysisTools/TextAnalyst.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <set>
 #include <unordered_map>
+#include <unordered_set>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -21,6 +22,7 @@ using std::cout;
 using std::string;
 using std::map;
 using std::unordered_map;
+using std::unordered_set;
 using std::pair;
 using std::vector;
 using std::ifstream ;
@@ -31,28 +33,30 @@ using std::function;
 using std::remove_if;
 
 /* 处理过一次文本后，提供查询方法：
- * 方法1：显示所有单词，以及出现次数
+ * 方法1：显示所有出现的单词，以及出现次数
  * 方法2：手动输入想要查询的单词（使用KMP以提供非精准查找方案）。将显示所有的可能结果。询问查询哪个单词（输入序号）。提供指定单词的出现行序与在各行的个数
- * 方法3：
+ * 方法3：显示所有找到的关键字
  *
 */
 class TextAnalyst {
-	map<string, int> wordsByDict; // 按字典序为单词排序
-//	vector<pair<string, int>> wordsByCount; // 按词频排序
-	map<string, map<int,int>> wordsLine; // 单词出现的行序号
-
+	map<string, int> wordsByDict;                // 词库。记录所有出现过的单词，key是单词，value是出现次数
+	map<string, map<int,int>> wordsLine;         // 单词与其出现的行序号与次数
+	vector<string> existing_keywords_sorted;     // 出现过的关键字，按词频降序存储
+	vector<string> existing_allWords_sorted;     // 出现了的单词，按词频降序存储
+	vector<string> keyWords;                     // 存储所有C++保留关键字
+	vector<string> zhushi;
 
 public:
-//	TextAnalyst();
+	TextAnalyst();                                           // 构造函数：加载keywords
 	void perform();                                          // 主函数入口
 	void initContainer();                                    // 初始化信息载体容器
-	void ShowAllWord_byMAPDICT() const;                      // 展示所有单词及其出现次数（从成员wordsByDict，按字典序）
-	void ShowAllWord_byVECTOR(vector<string>) const;         // 展示所有单词及其出现次数（从给定的向量，按字典序）
-	void InputSearch() const;                                // 输入想要查询的内容
-	void displayTheWord(const string&) const;                // 展示一个特定单词的出现行数与每行个数.参数确保存在
+	void ShowAllWord_byVECTOR(const vector<string>&) const;         // 功能实现接口：展示所有单词及其出现次数（从给定的数组中，按数组的顺序）
+	void InputSearch() const;                                // 功能2：输入想要查询的内容
+	void showKeyword() const;                                      // 功能3：展示保留字及其出现次数（按次数排序）
+	void displayTheWord(const string&) const;                // 功能实现接口：展示一个特定单词的出现行数与每行个数.参数确保存在
+	static bool trimLine2Word(string &line, string &word);   // 初始化信息用接口：将一行文本修减为一个单词，同时文本减去相应部分
 	static int myKMP(const string&, const string&) ;         // 实现一个KMP
-	static bool trimLine2Word(string &line, string &word);   // 将一行文本修建修建为一个单词，同时文本减去相应部分
-	void allClear();                                         // 清空所有容器
+	void clearAllContainer();                                // 清空所有容器
 
 };
 
@@ -60,12 +64,14 @@ public:
 #endif //TEXTANALYSISTOOLS_TEXTANALYST_H
 /*
  * bug日志：
- * 1、map无法以[]读取value
- * 2、重载出了问题
- * 3、输入流出错，检查所有输入输出的健壮性
+ * 1、map无法以[]读取value: const函数不行
+ * 2、重载出了问题: 那就不重载了
+ * 3、输入流出错，检查所有输入输出的健壮性 √
+ * 4、用文本2执行指令3时，会错误kmp出do和if。要拎出kmp来好好检查 √忘记清空新容器了
  *
  * 实现日志：
- * 1、识别_
- * 2、取消大小写区别√
- *
+ * 1. 识别 '_' √
+ * 2. 取消大小写区别√
+ * 3. 关键字未按字典序排列√
+ * 4. 功能2实现降序√
  */
